e046dac09aa441689d47467e0b4ed6e2d4eccce4
Tomson Kan <tomson.kan@gmail.com>
2020-01-08 11:16:55 -0600

    upload still fine tuning the delete function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import FirstComponent from './firstComponent'
+import { render } from '@testing-library/react';
 
 class App extends Component {
   constructor(props) {
@@ -35,15 +36,14 @@ formSubmit = event => {
   })
 }
 
-formSubmit = event => {
-  // built in method that does not wipe out state when browser refreshes
-  event.preventDefault()
-  console.log("***This is a State**", this.state.todos)
+onDelete = (index) => {
+  console.log("check " + index)
+  this.state.todos.splice(index,1)
   this.setState({
-    todos: [...this.state.todos, this.state.text],
-    text:' '
+    todos: [...this.state.todos].splice(index, 1)
   })
-}
+};
+
  //check
 render() {
 console.log("**THIS STATE**", this.state.isOn)
@@ -56,11 +56,25 @@ return (
      
       <form onSubmit = {this.formSubmit}>
     
-      <input value={this.state.text} onChange={this.inputUpdate}/>
+      <input 
+            value={this.state.text} 
+            onChange={this.inputUpdate}/>
       <button onClick = {this.handleClick}>Submit</button>  
       </form>
-      
-     <FirstComponent todos = {this.state.todos} />
+      <h1>To Do List (This is our listing Component) </h1>
+      <ul>
+        {this.state.todos.map((item, index) => {
+          return (
+          <li key={index}>
+          {this.state.todos[index]}
+          <button value = {index}onClick = {() => this.onDelete(item.index)}>Delete</button>
+          </li>
+          )
+        })}
+        </ul>
+     {/* <FirstComponent todos = {this.state.todos} /> */}
+     
+     
 
     </header>
     
@@ -70,3 +84,80 @@ return (
 }
 
 export default App;
+
+
+
+
+
+// class todoList extends Component {
+//   // Create a reference to grab input field
+//   textInput = React.createRef()
+
+//   // Create State object YAY, my prayers have been answered.
+//   state = {
+//     text: '',
+//     todos: [],
+//     isClicked: false,
+//   }
+
+//   // Function to set focus on text input Field
+//   componentDidMount() {
+//     this.textInput.current.focus();
+//   }
+
+//   // Actions taken when Add Todo button is clicked.
+//   addTodoButtonHandler = () => {
+//     // Save below 2 lines of code for another project
+//     // this.setState({ isClicked: !this.state.isClicked })
+//     // console.log(this.state.isClicked)
+    
+//     // Move submitted text to todos array
+//     this.state.todos.push(this.state.text);
+//     // console.log('addTodoButtonHandler() todo Push: ', this.state.todos);
+
+//     // Clear input field
+//     this.setState({ text: ''});
+
+//     // Set Focus on Text Input Field
+//     this.componentDidMount();
+//   }
+
+//   removeTask = (index)=> {
+//     // console.log(Object.prototype.toString.call(e.target.value))
+//     console.log(index)
+
+//     // const eValue = parseInt(e.target.value)
+//     // console.log('eValue***', eValue)
+
+//     console.log('todos state before splice***', this.state.todos)
+
+//     this.setState({
+//       todos: this.state.todos.splice(index, 1)
+//     })
+
+//     console.log('todos state after splice***', this.state.todos)
+
+//   }
+
+//   inputFieldText = (e) => {
+//     this.setState({ text: e.target.value })
+//     // console.log(e.target.value);
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.text}</h1>
+//         <button onClick={this.addTodoButtonHandler}>Add Item</button>
+//         <input ref={this.textInput} value={this.state.text} onChange={this.inputFieldText}></input>
+//         <ul>
+//           {this.state.todos.map((todo, index) => 
+//             <li key={index}>{todo}<button value={index} onClick={() => this.removeTask(index)}>Done!</button></li>
+//           )}
+//         </ul>
+//       </div>
+//     )
+//   }
+// };
+
+// export default todoList;
--- src/firstComponent.js
+++ src/firstComponent.js
@@ -1,18 +1,33 @@
-import React from 'react';
+// import React from 'react';
 
-function FirstComponent(props) {
-  console.log("This is the FirstComp props", props.todos)
-  return (
-    <div>
-    <h1>To Do List (This is our listing Component) </h1>
-    <ul>
-        {props.todos.map((item, index) => {
-          return <li key={index}>{props.todos[index]}</li>
-        
-        })}
-        </ul>
-    </div>    
-  )
-}
+// // onDelete = (index) => {
+// //   return this.setState({
+// //     todos: [...this.state.todos].splice(index, 1)
+// //   })
+// // }
 
-export default FirstComponent
+
+
+// function FirstComponent(props) {
+//   console.log("This is the FirstComp props", props.todos)
+//   return (
+//     <div>
+//     {/* <h1>To Do List (This is our listing Component) </h1> */}
+//     <ul>
+//         {props.todos.map((item, index) => {
+//           return (
+//           <li key={index}>{props.todos[index]}
+//           <button onClick = {() =>this.onDelete(item.index)}>Delete</button>
+//           </li>
+//           )
+//         })}
+//         </ul>
+//     </div>    
+//   )
+// }
+
+// export default FirstComponent
+
+// //onDelete = index => 
+// //find by index and create a new array w/out it
+// //<button onClick={() => this.onDelete(index)}>Delete</button>
